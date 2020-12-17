885313f1739e405f517a55493ad9bd8685241a8a
Gomez Elias <elias.f.g.61@gmail.com>
2020-12-17 16:18:13 -0500

    Elias Gomez Flores

--- ExamenParcial/app/src/main/java/com/example/examenparcial/MainActivity.java
+++ ExamenParcial/app/src/main/java/com/example/examenparcial/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.examenparcial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+ Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin = findViewById(R.id.button_inscripcion);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Registro.class);
+                startActivity(i);
+            }
+            });
     }
 }
